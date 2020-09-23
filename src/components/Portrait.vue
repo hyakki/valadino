96aeb6fae6f8ff7f4e7a7104db66c035ec370e48
Maxime Parisse <maximeparisse@gmail.com>
2020-09-23 14:43:33 +0200

    Play with displacement picture

--- src/components/Portrait.vue
+++ src/components/Portrait.vue
@@ -16,6 +16,8 @@ import { onMounted, ref, watch } from 'vue'
 import aladino from './../scripts/aladino'
 import gsap from 'gsap'
 
+import displacementPicture from './../assets/displacement.png'
+
 export default {
   name: 'Portrait',
   props: {
@@ -29,6 +31,7 @@ export default {
     const carpet = ref()
     const transform = ref(0.0)
     const displacement = ref(0.0)
+    const progress = ref(1.0)
 
     onMounted(() => {
       carpet.value = aladino.carpet(picture.value, {
@@ -37,14 +40,11 @@ export default {
             attribute vec2 position;
             attribute vec2 uv;
 
+            varying vec2 vUv;
+
             uniform mat4 projection;
             uniform float time;
             uniform float transform;
-            uniform float displacement;
- 
-            varying vec2 vUv;
-            varying float vTransform;
-            varying float vDisplacement;
 
             void main() {
               vec4 p = vec4(position, 0.0, 1.0);
@@ -52,8 +52,6 @@ export default {
               p.z += sin(uv.x * 1.0 + time * 0.0005) * transform;
 
               vUv = uv;
-              vTransform = transform;
-              vDisplacement = displacement;
 
               gl_Position = projection * p;
             }
@@ -62,12 +60,14 @@ export default {
             precision highp float;
 
             uniform sampler2D picture;
+            uniform sampler2D displacementPicture;
+            uniform float displacement;
+            uniform float progress;
+            uniform float time;
 
             varying vec2 vUv;
-            varying float vTransform;
-            varying float vDisplacement;
 
-            vec4 displace(sampler2D picture, vec2 uv, float dis) {
+            vec4 displaceF(sampler2D picture, vec2 uv, float dis) {
               float red = texture2D(picture, vec2(uv.x - dis, uv.y + dis)).r;
               float green = texture2D(picture, vec2(uv.x + dis, uv.y - dis)).g;
               float blue = texture2D(picture, vec2(uv.x, uv.y)).b;
@@ -77,24 +77,34 @@ export default {
             }
 
             void main() {
-              vec2 uv = vUv;
-              float dis = vDisplacement;
+              // Displacement test
+              // vec4 color = displaceF(picture, vUv, progress / 10.0);
 
-              vec4 color = displace(picture, uv, dis);
+              vec4 displace = texture2D(displacementPicture, vUv.yx);
+              vec2 displacedUV = vec2(vUv.x, vUv.y);
+
+              displacedUV.y = mix(vUv.y, displace.r - 0.15, progress);
+
+              vec4 color = texture2D(picture, displacedUV);
+
+              float coef = sin(time / 1000.0);
+
+              color.r = texture2D(picture, displacedUV - vec2(0.0, 0.01) * progress * coef).r;
+              color.g = texture2D(picture, displacedUV - vec2(0.0, 0.02) * progress * coef).g;
+              color.b = texture2D(picture, displacedUV - vec2(0.0, 0.03) * progress * coef).b;
 
               gl_FragColor = color;
             }
           `,
         }), 
         uniforms: {
-          picture: aladino.texture({
-            url: picture.value.src,
-          }),
+          picture: aladino.texture(picture.value.src),
+          displacementPicture: aladino.texture(displacementPicture),
           transform: transform.value,
           displacement: displacement.value,
+          progress: progress.value,
         },
       })
-
     })
 
     const onOver = () => {
@@ -104,6 +114,13 @@ export default {
       gsap.to(displacement, {
         duration: 0.7,
         value: 0.03,
+        ease: 'power4.out',
+      })
+
+      gsap.killTweensOf(progress)
+      gsap.to(progress, {
+        duration: 0.3,
+        value: 0.0,
         ease: 'power4.out',
       })
     }
@@ -117,13 +134,21 @@ export default {
         value: 0.0,
         ease: 'power4.in',
       })
+
+      gsap.killTweensOf(progress)
+      gsap.to(progress, {
+        duration: 0.2,
+        value: 1.0,
+        ease: 'power4.in',
+      })
     }
 
     watch(
-      () => [displacement.value, transform.value],
+      () => [displacement.value, transform.value, progress.value],
       v => {
         carpet.value.uniforms.displacement = v[0]
         carpet.value.uniforms.transform = v[1]
+        carpet.value.uniforms.progress = v[2]
       }
     )
 
@@ -139,6 +164,8 @@ export default {
 <style>
 .portrait-inner {
   position: relative;
+  border: 1px solid black;
+  cursor: pointer;
 }
 
 .portrait-inner::before {
--- src/scripts/aladino/index.ts
+++ src/scripts/aladino/index.ts
@@ -1,6 +1,7 @@
-import Aladino from 'aladino'
+import Aladino from './../vendors/aladino'
 
 const aladino = new Aladino()
+
 document.body.appendChild(aladino.canvas)
 
 export default aladino
